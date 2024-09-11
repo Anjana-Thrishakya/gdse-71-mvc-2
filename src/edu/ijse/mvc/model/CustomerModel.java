ebdab118b708b4410fc489d4357b2ea2ca93aa98
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-09-11 12:25:55 +0530

    Save Customer Model

---
+++ src/edu/ijse/mvc/model/CustomerModel.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.model;
+
+import edu.ijse.mvc.db.DBConnection;
+import edu.ijse.mvc.dto.CustomerDto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author anjan
+ */
+public class CustomerModel {
+    
+    public String saveCustomer(CustomerDto customerDto) throws Exception{
+        Connection connection = DBConnection.getInstance().getConnection();
+        String sql = "INSERT INTO Customer VALUES (?,?,?,?,?,?,?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        
+        statement.setString(1, customerDto.getId());
+        statement.setString(2, customerDto.getTitle());
+        statement.setString(3, customerDto.getName());
+        statement.setString(4, customerDto.getDob());
+        statement.setDouble(5, customerDto.getSalary());
+        statement.setString(6, customerDto.getAddress());
+        statement.setString(7, customerDto.getCity());
+        statement.setString(8, customerDto.getProvince());
+        statement.setString(9, customerDto.getPostalCode());
+        
+        int result = statement.executeUpdate();
+        return result > 0 ? "Successfully Saved" : "Fail";
+    }
+    
+}
